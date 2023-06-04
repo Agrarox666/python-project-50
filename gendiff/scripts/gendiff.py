dd38878759cd32a478900014ba7dd31c22cf58b9
Igor Loskutov <omfi017@gmail.com>
2023-06-04 18:25:11 +0500

    add generate_diff func in gendiff module

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
-import
+import json
+
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
@@ -14,3 +15,25 @@ if __name__ == '__main__':
     main()
 
 def generate_diff(file_path1, file_path2):
+
+    json1 = json.load(open(file_path1))
+    json2 = json.load(open(file_path2))
+
+    keys = set(json1.keys())
+    keys.update(json2.keys())
+    keys = sorted(keys)
+    result = '{'
+
+    for key in keys:
+        if key in json1 and key in json2:
+            if json1[key] == json2[key]:
+                result = f'{result}\n   {key}: {json1[key]}'
+            else:
+                result = f'{result}\n - {key}: {json1[key]}\n + {key}: {json2[key]}'
+        elif key in json1:
+            result = f'{result}\n - {key}: {json1[key]}'
+        else:
+            result = f'{result}\n + {key}: {json2[key]}'
+    result += '\n}'
+
+    return result
