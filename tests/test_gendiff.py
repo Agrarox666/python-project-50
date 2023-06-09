0605be26291f1a2592e2460297d2b20e3e41f937
Igor Loskutov <omfi017@gmail.com>
2023-06-10 01:04:14 +0500

    one more attempt

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,8 @@
 from gendiff.scripts.gendiff import generate_diff
 def test_gendiff():
 
-    file_path1 = 'fixtures/file1.json'
-    file_path2 = 'fixtures/file2.json'
-    file_path_expected = 'fixtures/expected1.txt'
+    file_path1 = 'tests/fixtures/file1.json'
+    file_path2 = 'tests/fixtures/file2.json'
+    file_path_expected = 'tests/fixtures/expected1.txt'
 
     assert generate_diff(file_path1, file_path2) == (open(file_path_expected).read())
