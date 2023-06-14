9991e4013fadd5efcf2774e7eccefa6d50683805
Igor Loskutov <omfi017@gmail.com>
2023-06-14 16:08:50 +0500

    add some fixtures for 6 step

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,5 @@
 from gendiff.scripts.gendiff import generate_diff
-from gendiff.scripts.gendiff import parser
+
 def test_generate_diff():
 
     file_path1 = 'tests/fixtures/file1.json'
@@ -15,3 +15,10 @@ def test_parser():
     file_path_expected = 'tests/fixtures/expected2.txt'
 
     assert generate_diff(file_path1, file_path2) == (open(file_path_expected).read())
+
+def test_deep_diff():
+
+    file_path3 = 'tests/fixtures/file3.json'
+    file_path4 = 'tests/fixtures/file4.json'
+    file_path_expected = 'tests/fixtures/expected3.txt'
+    assert generate_diff(file_path3, file_path4) == (open(file_path_expected).read())
