7435b0a1185c44c8ff3e27517500d9f11dcb3f41
Igor Loskutov <omfi017@gmail.com>
2023-06-27 18:01:11 +0500

    fix some problems

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,13 +1,10 @@
 import argparse
 
-from gendiff.create_diff import generate_diff
-
-
-def main():
+def get_cli_arguments():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    return generate_diff(args.first_file, args.second_file, args.format)
+    return args.first_file, args.second_file, args.format
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,8 @@
-from gendiff.cli import main
+from gendiff.cli import get_cli_arguments
+from gendiff.create_diff import generate_diff
+
+def main():
+    print(generate_diff(*get_cli_arguments()))
 
 if __name__ == '__main__':
-    print(main())
+    main()
