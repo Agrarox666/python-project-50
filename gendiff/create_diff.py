46485e19b2383d25d40db3c26462d03656a322b4
Igor Loskutov <omfi017@gmail.com>
2023-06-27 18:08:32 +0500

    fix linter bugs and move generate_diff to gendiff.py

--- gendiff/create_diff.py
+++ gendiff/create_diff.py
@@ -1,11 +1,4 @@
 # flake8: noqa: C901
-import json
-import yaml
-
-from gendiff.clean_booleans import format_bool_from_Python_to_Json
-from gendiff.formatters.json_formatter import json_formatter
-from gendiff.formatters.plain_formatter import plain
-from gendiff.formatters.stylish_formatter import stylish
 
 
 def create_diff(file1: dict, file2: dict):
@@ -36,22 +29,3 @@ def create_diff(file1: dict, file2: dict):
     return walk(file1, file2, 1)
 
 
-def generate_diff(file_path1, file_path2, formatter='stylish'):
-    if file_path1.endswith('.json') and file_path2.endswith('.json'):
-        file1 = json.load(open(file_path1))
-        file2 = json.load(open(file_path2))
-
-    elif file_path1.endswith('.yml') and file_path2.endswith('.yml'):
-        file1 = yaml.load(open(file_path1), Loader=yaml.FullLoader)
-        file2 = yaml.load(open(file_path2), Loader=yaml.FullLoader)
-    else:
-        raise Exception
-
-    format_bool_from_Python_to_Json(file1)
-    format_bool_from_Python_to_Json(file2)
-    if formatter == 'stylish':
-        return stylish(create_diff(file1, file2))
-    elif formatter == 'plain':
-        return plain(create_diff(file1, file2))
-    elif formatter == 'json':
-        return json_formatter(create_diff(file1, file2))
