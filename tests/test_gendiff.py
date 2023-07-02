a89207b89966e43227510a07f0faf788e8fbffa2
Igor Loskutov <omfi017@gmail.com>
2023-07-02 19:07:47 +0500

    fixing linter bugs

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -30,6 +30,7 @@ def test_plain_diff():
     file_path_expected = 'tests/fixtures/expected4.txt'
     assert generate_diff(file_path3, file_path4, 'plain') == (open(file_path_expected).read())
 
+
 def test_json_diff():
     file_path3 = 'tests/fixtures/file3.json'
     file_path4 = 'tests/fixtures/file4.json'
